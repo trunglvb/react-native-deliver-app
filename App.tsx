ecef0e318ca14d94cc64265c8589354ee67f5d70
trunglvb <levubaotrung0311@gmail.com>
2023-09-09 09:44:32 +0700

    setting project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
+interface IState {
+  name: string
+  age: string
+}
 export default function App() {
+  const [info, setInfo] = useState<IState>({
+    name: 'ne trung',
+    age: '10'
+  })
+  const expr: any = 'Papayas'
+  switch (expr) {
+    case '1':
+      console.log('Oranges are $0.59 a pound.')
+      break
+
+    default:
+      console.log(`Sorry, we are out of ${expr}.`)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{info.name}</Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
